be9ca6ada913a56cb861102efb40494c64aeda3f
BrigadierBaobab <andreea.lutac@gmail.com>
2015-03-17 14:29:47 +0000

    Bla

--- scaleTEST.py
+++ scaleTEST.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 
 from operator import itemgetter
-#Imposes a range of 0.35 <-> 0.75 on the x-axis and -0.4 <-> 0.4 on the y-axis, to conform to Baxter's strange coordinate system.
+#Imposes a range of 0.4 <-> 0.7 on the x-axis and -0.4 <-> 0.4 on the y-axis, to conform to Baxter's strange coordinate system.
 newtok = [0,0,0,0]
 current_pos = [0,0]
 points = []
 distances = []
 
-file1 = open("lines2.txt")
+file1 = open("andreeaFinalProcessedTableCloth.txt")
 file2 = open('scaledLinesTEST.txt','w')
 file3 = open("lines2TEST.txt",'w')
 for line in file1:
@@ -17,14 +17,14 @@ for line in file1:
 		continue
 	#print tokens
 	tokens[3] = tokens[3][:len(tokens[3])-1]
-	xRange = float(5.0/6.0) * 0.45
-	xOffset = (0.8 - xRange)/2
+	xRange = float(5.0/6.0) * 0.3
+	xOffset = 0.2#float((0.7 - xRange)/2)
 	file3.write("("+tokens[0]+","+tokens[1]+")\n("+tokens[2]+","+tokens[3]+")\n")
 
-	newtok[0] = (xRange*float(tokens[0]))/500 - xOffset# - 0.4
-	newtok[1] = (0.4*float(tokens[1]))/600 + 0.35
-	newtok[2] = (xRange*float(tokens[2]))/500 - xOffset# - 0.4
-	newtok[3] = (0.4*float(tokens[3]))/600 + 0.35
+	newtok[0] = (xRange*float(tokens[0]))/500 - 0.1# - 0.4
+	newtok[1] = (0.3*float(tokens[1]))/600 + 0.4
+	newtok[2] = (xRange*float(tokens[2]))/500 - 0.1# - 0.4
+	newtok[3] = (0.3*float(tokens[3]))/600 + 0.4
 
 	### Preliminary reduction ###
 	dist = ((newtok[2]-newtok[0]) ** 2 + (newtok[3]-newtok[1]) ** 2) ** 0.5
@@ -44,27 +44,28 @@ for line in file1:
 
 
 ### REDUCE NUMBER OF POINTS ###
-pcopy = list(points)
-pcopy.sort(key=itemgetter(0))
+# pcopy = list(points)
+# pcopy.sort(key=itemgetter(0))
 
-print len(points)
-d = 0
-for i in xrange(0,len(pcopy)-1):
-	for j in xrange(i+1,len(pcopy)):
-		# print i,j,pcopy[i], pcopy[j]
-		if pcopy[j][2] == "u":
-			dist = float(((pcopy[j][0]-pcopy[i][0]) ** 2 + (pcopy[j][1]-pcopy[i][1]) ** 2) ** 0.5)
+# print len(points)
+# d = 0
+# for i in xrange(0,len(pcopy)-1):
+# 	for j in xrange(i+1,len(pcopy)):
+# 		# print i,j,pcopy[i], pcopy[j]
+# 		if pcopy[j][2] == "u":
+# 			dist = float(((pcopy[j][0]-pcopy[i][0]) ** 2 + (pcopy[j][1]-pcopy[i][1]) ** 2) ** 0.5)
 
-			if dist < 0.0035:
-				d+=1
-				points.remove(pcopy[j])
-				pcopy[j][2] = "d"
-print "Deleted: "
-print d
+# 			if dist < 0.001:
+# 				d+=1
+# 				points.remove(pcopy[j])
+# 				pcopy[j][2] = "d"
+# print "Deleted: "
+# print d
 print len(points)
 # print points
 for p in points:
-	file2.write(str(p[0])+","+str(p[1])+"\n")
+	if p[0]<=0.12 and p[0]>=-0.065:
+		file2.write(str(p[0])+","+str(p[1])+"\n")
 
 
 file1.close()
